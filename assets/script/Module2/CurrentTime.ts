ea103c6056dd81e2191f97d227de1569c794dbf3
TKNW <kyon1.048596@gmail.com>
2024-04-22 23:09:02 +0800

    Fix time format

--- assets/script/Module2/CurrentTime.ts
+++ assets/script/Module2/CurrentTime.ts
@@ -17,8 +17,9 @@ export class CurrentTime extends Component {
         const date = new Date();
         const ampm = date.getHours() <= 12 ? '上午' : '下午';
         const hour = date.getHours() > 12 ? date.getHours() - 12: date.getHours();
+        const minute = date.getMinutes() < 10 ? `0${date.getMinutes().toString()}` : date.getMinutes().toString();
         const second = date.getSeconds() < 10 ? `0${date.getSeconds().toString()}` : date.getSeconds().toString();
-        this.timeLabel.string = `現在時間：${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${ampm}${hour}:${date.getMinutes()}:${second}`;
+        this.timeLabel.string = `現在時間：${date.getFullYear()}/${date.getMonth()+1}/${date.getDate()} ${ampm}${hour}:${minute}:${second}`;
     }
     protected update(dt: number): void {
         this.updateCurrentTime();
